ad45683f32a65e0384f62d3d180ceddb9755429f
Konrád Kaiser <konrad.kaiser@stud.uni-corvinus.hu>
2019-11-30 21:42:06 +0100

    Commit3

--- hetedikhazi/hetedikhazi/Form1.cs
+++ hetedikhazi/hetedikhazi/Form1.cs
@@ -12,12 +12,24 @@ namespace hetedikhazi
 {
     public partial class Form1 : Form
     {
+        BindingList<Entities.User> users = new BindingList<Entities.User>();
         public Form1()
         {
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+
             InitializeComponent();
             label1.Text = Resource1.LastName;
             label2.Text = Resource1.FirstName;
             button1.Text = Resource1.Add;
+
+            var u = new Entities.User()
+            {
+                FullName = textBox1.Text,
+                
+            };
+            users.Add(u);
         }
     }
 }
