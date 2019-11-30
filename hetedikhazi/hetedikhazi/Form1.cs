fcf2f54024d82bc44a1a390ec6e83ad4315000d6
Konrád Kaiser <konrad.kaiser@stud.uni-corvinus.hu>
2019-11-30 21:14:47 +0100

    Commit2

---
+++ hetedikhazi/hetedikhazi/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hetedikhazi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = Resource1.LastName;
+            label2.Text = Resource1.FirstName;
+            button1.Text = Resource1.Add;
+        }
+    }
+}
