0064186a1c61ea0baa2722d77c06b6d14c1b4d60
Konrád Kaiser <konrad.kaiser@stud.uni-corvinus.hu>
2019-12-02 21:52:57 +0100

    commit4

--- hetedikhazi/hetedikhazi/Form1.cs
+++ hetedikhazi/hetedikhazi/Form1.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace hetedikhazi
 {
     public partial class Form1 : Form
     {
-        BindingList<Entities.User> users = new BindingList<Entities.User>();
+        private BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             listBox1.DataSource = users;
@@ -21,15 +23,38 @@ namespace hetedikhazi
 
             InitializeComponent();
             label1.Text = Resource1.LastName;
-            label2.Text = Resource1.FirstName;
+
+            button2.Text = "Fájlba írás";
+            
+
             button1.Text = Resource1.Add;
 
-            var u = new Entities.User()
+            
+            var u = new User()
             {
                 FullName = textBox1.Text,
                 
             };
             users.Add(u);
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd1 = new SaveFileDialog();
+            if (sfd1.ShowDialog() != DialogResult.OK) return;
+            using StreamWriter sw = new StreamWriter(sfd1.FileName, false, Encoding.UTF8);
+            {
+                foreach (var s in users)
+                {
+                    sw.WriteLine(s.ID);
+                    sw.WriteLine(";");
+                    sw.WriteLine(s.FullName);
+                    sw.WriteLine();
+
+                }
+
+            }
+        }
+        }
     }
-}
+
